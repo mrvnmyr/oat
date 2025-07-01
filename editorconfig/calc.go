f2544b0a192a66108ae02b97850515adfd652553
Marvin Mayer <mrvnmyr@protonmail.com>
2025-07-01 14:59:25 +0200

    editorconfig: Support indexing key via 2nd arg

--- editorconfig/calc.go
+++ editorconfig/calc.go
@@ -3,18 +3,36 @@ package editorconfig
 import (
 	"encoding/json"
 	"fmt"
+	"os"
 
 	"github.com/editorconfig/editorconfig-core-go/v2"
 	"github.com/mrvnmyr/oat/common"
 )
 
-func Calc(path string) {
+func Calc(path string, key string) {
 	def, err := editorconfig.GetDefinitionForFilename(path)
 	common.Check(err)
 
 	{ // print as JSON
-		fooOutput, err := json.Marshal(&def)
-		common.Check(err)
-		fmt.Printf("%s\n", fooOutput)
+		if key != "" {
+			result := make(map[string]interface{})
+
+			jsonOutput, err := json.Marshal(def)
+			common.Check(err)
+
+			err = json.Unmarshal(jsonOutput, &result)
+			common.Check(err)
+
+			if val, ok := result[key]; ok {
+				fmt.Printf("%s\n", val)
+			} else {
+				// fmt.Fprintf(os.Stderr, "Unknown key: %s\n", key)
+				os.Exit(1)
+			}
+		} else {
+			jsonOutput, err := json.Marshal(&def)
+			common.Check(err)
+			fmt.Printf("%s\n", jsonOutput)
+		}
 	}
 }
