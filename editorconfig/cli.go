78b1b55191f5d1ddb44f2f6700a4423556b17f2b
Marvin Mayer <mrvnmyr@protonmail.com>
2025-07-27 19:42:12 +0200

    editorconfig: Support indexing key via 2nd arg

--- editorconfig/cli.go
+++ editorconfig/cli.go
@@ -3,10 +3,14 @@ package editorconfig
 import "github.com/spf13/cobra"
 
 var Cmd = &cobra.Command{
-	Use:  "editorconfig [path-to-any-file]",
-	Args: cobra.ExactArgs(1),
+	Use:  "editorconfig <path-to-any-file> [optional-key]",
+	Args: cobra.RangeArgs(1, 2),
 	Run: func(cmd *cobra.Command, args []string) {
-		Calc(args[0])
+		if len(args) == 1 {
+			Calc(args[0], "")
+		} else {
+			Calc(args[0], args[1])
+		}
 	},
 }
 
